0da4814926efc6c593dce83ae367e3167ec81cc2
Gentjana <hanigentjana@gmail.com>
2026-02-05 13:07:38 +0100

    add_pratica

---
+++ src/test/java/gentjanahani/u2w5d4/U2w5d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package gentjanahani.u2w5d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w5d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
